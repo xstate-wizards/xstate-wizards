b343a5e5d6962220ba723c0c05f0da068d20e04e
Mark Hansen <x@markthemark.com>
2022-08-09 18:48:17 -0400

    fix(spells): acorn.parse ecmaVersion 2020

--- packages/spells/src/machines/functions/templateFunctionToValue.ts
+++ packages/spells/src/machines/functions/templateFunctionToValue.ts
@@ -1,4 +1,5 @@
 import { parse } from "acorn";
+import { cloneDeep } from "lodash";
 import { $TSFixMe, TWizardSerializations } from "../../types";
 import { logger } from "../../wizardDebugger";
 import { evalJsonLogic } from "./evalJsonLogic";
@@ -17,17 +18,22 @@ export type TSelectStatementToValue = {
 // Eval
 export const templateFunctionToValue = ({ ctx, string, functions, contentTree }: TSelectStatementToValue): any => {
   const stringStripped = isTemplateFunctionPattern(string) ? cleanTemplateFunction(string) : string;
-  logger.debug("templateFunctionToValue: ", { string, stringStripped });
-  const parsed = parse(stringStripped, { ecmaVersion: "latest" });
-  // FN NAME
-  // @ts-ignore
-  const callee = parsed.body?.[0]?.expression?.callee?.name;
-  // ARGS (if json-logic, this is a JSON.stringified() obj)
-  // @ts-ignore
-  const args = parsed.body?.[0]?.expression?.arguments?.map((a) => a?.value) ?? [];
-  logger.debug("templateFunctionToValue: ", { callee, args, parsed });
-  // Return (handle JSON_LOGIC special, cause we want to pass in extra data)
-  return callee === "JSON_LOGIC"
-    ? evalJsonLogic(JSON.parse(args), { context: ctx, content: contentTree ?? {} })
-    : functions[callee]?.(ctx, ...args);
+  logger.debug("templateFunctionToValue: ", cloneDeep({ ctx, functions, contentTree, string, stringStripped }));
+  try {
+    const parsed = parse(stringStripped, { ecmaVersion: 2020 }); // ecmaVersion: "latest" hangs
+    // FN NAME
+    // @ts-ignore
+    const callee = parsed.body?.[0]?.expression?.callee?.name;
+    // ARGS (if json-logic, this is a JSON.stringified() obj)
+    // @ts-ignore
+    const args = parsed.body?.[0]?.expression?.arguments?.map((a) => a?.value) ?? [];
+    logger.debug("templateFunctionToValue: ", { callee, args, parsed });
+    // Return (handle JSON_LOGIC special, cause we want to pass in extra data)
+    return callee === "JSON_LOGIC"
+      ? evalJsonLogic(JSON.parse(args), { context: ctx, content: contentTree ?? {} })
+      : functions[callee]?.(ctx, ...args);
+  } catch (e) {
+    logger.error("templateFunctionToValue: ", { string, stringStripped });
+    return undefined;
+  }
 };
