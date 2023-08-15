83ffbf81d0bbcf2fae2d6fc3bc9a5eb070f5e8cb
Ruslan Vasylenko <r.vasylenko@jssolutionsdev.com>
2023-08-15 14:30:51 +0300

    improve: wizards-of-react: InputPhoneNumber: add property allowCountryCode, if false, replace country code select with default text "+1"

--- packages/wizards-of-react/src/components/contentNodes/InputPhoneNumber.tsx
+++ packages/wizards-of-react/src/components/contentNodes/InputPhoneNumber.tsx
@@ -10,9 +10,16 @@ type TInputPhoneNumberProps = {
   onChange: (value: string) => void;
   size?: TComponentSize;
   value: string;
+	allowCountryCode?: boolean;
 };
 
-export const InputPhoneNumber: React.FC<TInputPhoneNumberProps> = ({ disabled, onChange, size, value }) => {
+export const InputPhoneNumber: React.FC<TInputPhoneNumberProps> = ({
+  disabled,
+  onChange,
+  size,
+  value,
+  allowCountryCode = true,
+}) => {
   const [countryCode, setCountryCode] = useState(`+${parseTel(value).getCountryCode ?? "1"}`);
   const [phoneNumber, setPhoneNumber] = useState(parseTel(value)?.getNationalNumber ?? "");
   useEffect(() => {
@@ -26,19 +33,23 @@ export const InputPhoneNumber: React.FC<TInputPhoneNumberProps> = ({ disabled, o
   // RENDER
   return (
     <StyledInputPhoneNumber>
-      {/* @ts-ignore */}
-      <Select disabled={disabled} size={size} value={countryCode} onChange={(e) => setCountryCode(e.target.value)}>
-        {Object.keys(COUNTRY_CALLING_CODES)
-          .sort()
-          .sort((a) => (a === "US" ? -1 : 0))
-          .map((country) => (
-            // @ts-ignore
-            <option key={country} value={COUNTRY_CALLING_CODES[country]}>
-              {/* @ts-ignore */}
-              {country} {COUNTRY_CALLING_CODES[country]}
-            </option>
-          ))}
-      </Select>
+	    {allowCountryCode ? (
+      // @ts-ignore
+        <Select disabled={disabled} size={size} value={countryCode} onChange={(e) => setCountryCode(e.target.value)}>
+          {Object.keys(COUNTRY_CALLING_CODES)
+            .sort()
+            .sort((a) => (a === "US" ? -1 : 0))
+            .map((country) => (
+              // @ts-ignore
+              <option key={country} value={COUNTRY_CALLING_CODES[country]}>
+                {/* @ts-ignore */}
+                {country} {COUNTRY_CALLING_CODES[country]}
+              </option>
+            ))}
+        </Select>
+	    ) : (
+        <StyledUSCode>+1</StyledUSCode>
+      )}
       <Input
         // @ts-ignore
         size={size}
@@ -65,4 +76,14 @@ export const StyledInputPhoneNumber = styled.div`
     margin: 0;
     margin-bottom: 0 !important; // YIKES
   }
+`;
+
+const StyledUSCode = styled.span`
+  display: flex;
+  align-items: center;
+  font-weight: normal;
+  white-space: nowrap;
+  font-size: 16px;
+  min-height: 1.2em;
+  padding: 0px 16px;
 `;
