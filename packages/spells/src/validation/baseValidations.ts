60acadb51c7fe1ac4a7d0d929b0c3997155a1fc8
Mark <x@markthemark.com>
2026-03-03 23:43:40 -0500

    fix(spells): validation typo

--- packages/spells/src/validation/baseValidations.ts
+++ packages/spells/src/validation/baseValidations.ts
@@ -1,12 +1,12 @@
 import { parseTel } from "tel-fns";
 import zxcvbn from "zxcvbn";
 import { TValidationMap } from "../types";
-import { ValdidationTypes } from "../constants/validationConstants";
+import { ValidationTypes } from "../constants/validationConstants";
 import { logger } from "../wizardDebugger";
 
 // VALIDATIONS METHODS
 export const baseValidations: TValidationMap = {
-  [ValdidationTypes.required]: (value) => {
+  [ValidationTypes.required]: (value) => {
     const validateRequired = () => {
       if (value == null) return false;
       if (typeof value === "string" && value.length === 0) return false;
@@ -17,27 +17,27 @@ export const baseValidations: TValidationMap = {
     };
     return !validateRequired() ? "Required." : null;
   },
-  [ValdidationTypes.positiveNumber]: (value) => {
+  [ValidationTypes.positiveNumber]: (value) => {
     const validatePositiveNumber = () => value > 0;
     return !validatePositiveNumber() ? "Must be higher than 0." : null;
   },
-  [ValdidationTypes.zeroOrGreaterNumber]: (value) => {
+  [ValidationTypes.zeroOrGreaterNumber]: (value) => {
     const validateZeroOrGreaterNumber = () => value >= 0;
     return !validateZeroOrGreaterNumber() ? "Must be 0 or higher." : null;
   },
-  [ValdidationTypes.isInteger]: (value) => {
+  [ValidationTypes.isInteger]: (value) => {
     const validateInteger = () => value % 1 === 0;
     return !validateInteger() ? "Cannot be a decimal." : null;
   },
-  [ValdidationTypes.isTrue]: (value) => {
+  [ValidationTypes.isTrue]: (value) => {
     const validateTrueBoolean = () => value === true;
     return !validateTrueBoolean() ? "Must be true." : null;
   },
-  [ValdidationTypes.isFalse]: (value) => {
+  [ValidationTypes.isFalse]: (value) => {
     const validateFalseBoolean = () => value === false;
     return !validateFalseBoolean() ? "Must be false." : null;
   },
-  [ValdidationTypes.password]: (value) => {
+  [ValidationTypes.password]: (value) => {
     const validatePassword = (str) => {
       // at least 6 characters
       if (str.length <= 6) return "Passwords must contain more than 6 characters.";
@@ -47,12 +47,12 @@ export const baseValidations: TValidationMap = {
     };
     return validatePassword(String(value));
   },
-  [ValdidationTypes.validEmail]: (value, validations) => {
+  [ValidationTypes.validEmail]: (value, validations) => {
     // TODO: make better
     const validateEmail = () => value?.includes("@") && value?.includes(".") && String(value)?.length > 5;
     return !validateEmail() ? "Invalid email." : null;
   },
-  [ValdidationTypes.validPhoneNumber]: (value, validations) => {
+  [ValidationTypes.validPhoneNumber]: (value, validations) => {
     // TODO: can be a bit smarter with this check + need to handle international?
     const parsed = parseTel(value)
     logger.debug("validateInputValue (validPhoneNumber):", { parsed, value });
@@ -60,15 +60,15 @@ export const baseValidations: TValidationMap = {
       (!validations.includes("required") && !value) || parsed?.isValidNumber;
     return !validatePhoneNumber() ? "Invalid phone number." : null;
   },
-  [ValdidationTypes.isChecked]: (value) => {
+  [ValidationTypes.isChecked]: (value) => {
     const validateTrueBoolean = () => value === true;
     return !validateTrueBoolean() ? "Must be checked." : null;
   },
-  [ValdidationTypes.isNotChecked]: (value) => {
+  [ValidationTypes.isNotChecked]: (value) => {
     const validateFalseBoolean = () => value === false;
     return !validateFalseBoolean() ? "Must not be checked." : null;
   },
-  [ValdidationTypes.fullName]: (value, validations) => {
+  [ValidationTypes.fullName]: (value, validations) => {
     const validateFullName = () =>
       (!validations.includes("required") && !value) || value.split(" ").filter((name) => name.length > 0).length >= 2;
     return !validateFullName() ? "Must enter first and last name." : null;
--- packages/spells/src/validation/regexValidations.ts
+++ packages/spells/src/validation/regexValidations.ts
@@ -1,7 +1,7 @@
-import { ValdidationTypes } from "../constants/validationConstants";
+import { ValidationTypes } from "../constants/validationConstants";
 
 export const regexValidations = {
-  [ValdidationTypes.stringLength]: (value, validations, param) => {
+  [ValidationTypes.stringLength]: (value, validations, param) => {
     const passed = (value || "").length === Number(param);
     return !passed ? `Expecting ${param} characters.` : null;
   },
